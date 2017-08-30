6aa803bc01631098b3f1490108ff9ca720b735b9
Sebas95 <sgonzalezquesada1@gmail.com>
2017-08-30 09:06:54 -0600

    update

--- ProyectoQuartus/db/ip/sld1bdf8663/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ ProyectoQuartus/db/ip/sld1bdf8663/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : f75b62e77dd13a431dcd
+//   DESIGN_HASH   : 3f81d57878668d09c3c5
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf75b62e77dd13a431dcd };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h3f81d57878668d09c3c5 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
