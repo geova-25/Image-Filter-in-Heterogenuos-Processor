ebbd667e99d1042f9a530cbb7f9b14dfc38ad369
Sebas95 <sgonzalezquesada1@gmail.com>
2017-09-11 20:21:40 -0600

    update

---
+++ ProyectoQuartus/software/Image_Filter/obj/default/slaveProcessorFilter.d
@@ -0,0 +1,9 @@
+obj/default/slaveProcessorFilter.o: slaveProcessorFilter.c \
+ ../Image_Filter_bsp//HAL/inc/sys/alt_stdio.h \
+ ../Image_Filter_bsp/system.h ../Image_Filter_bsp/linker.h
+
+../Image_Filter_bsp//HAL/inc/sys/alt_stdio.h:
+
+../Image_Filter_bsp/system.h:
+
+../Image_Filter_bsp/linker.h:
